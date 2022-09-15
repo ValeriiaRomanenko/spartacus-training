57388cea839f893fcba84860d51d218818114b2b
Iegor Funtusov <iegor_funtusov@epam.com>
2022-09-15 20:03:01 +0300

    add product listing page

--- spartacustraining-frontend/src/app/spartacus/spartacus-training.module.ts
+++ spartacustraining-frontend/src/app/spartacus/spartacus-training.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from "../shared/shared.module";
-import { LayoutModule } from "../layout/layout.module";
 import {
   RegisterModule,
   StCartModule,
