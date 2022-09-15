462894db8d75d99ca5890bc99c62818dce1c929d
Iegor Funtusov <iegor_funtusov@epam.com>
2022-09-15 14:08:22 +0300

    Rebase Unit 1 functionality

--- spartacustraining-frontend/src/app/spartacus/spartacus-training.module.ts
+++ spartacustraining-frontend/src/app/spartacus/spartacus-training.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterModule, StCartModule } from '../pages';
 import { SharedModule } from "../shared/shared.module";
 import { LayoutModule } from "../layout/layout.module";
-import { RegisterModule, StCartModule } from '../pages';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    RegisterModule,
     SharedModule,
     RegisterModule,
     LayoutModule,
