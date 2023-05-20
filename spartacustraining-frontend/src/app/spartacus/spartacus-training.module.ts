fb9d630c9a616e224c223097844a7c754d91b745
Valeriia Romanenko <valeriia_romanenko@epam.com>
2023-05-20 19:39:21 +0300

    [unit-1] customize header and registration page

--- spartacustraining-frontend/src/app/spartacus/spartacus-training.module.ts
+++ spartacustraining-frontend/src/app/spartacus/spartacus-training.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from "../shared/shared.module";
+import { LayoutModule } from "../layout/layout.module";
 import {
   RegisterModule,
   StCartModule,
   StOrderSummaryModule,
   StPlpModule
 } from '../pages';
-import { SharedModule } from "../shared/shared.module";
-import { LayoutModule } from "../layout/layout.module";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RegisterModule,
     SharedModule,
     RegisterModule,
     LayoutModule,
